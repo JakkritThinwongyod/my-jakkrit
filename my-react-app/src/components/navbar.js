33ce00b28cfff9802ff42322c27157c25459dce4
Jakkrit Thinwongyod <ainookkub@gmail.com>
2023-06-14 16:22:54 +0700

    Update Customer

--- my-react-app/src/components/navbar.js
+++ my-react-app/src/components/navbar.js
@@ -1,10 +1,6 @@
 import { Outlet} from "react-router-dom";
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-
+import {Container,Nav,Navbar,Offcanvas } from 'react-bootstrap';
 
 const Navbars=()=> {
   return (
--- my-react-app/src/components/nookShabu/NsCustomer.js
+++ my-react-app/src/components/nookShabu/NsCustomer.js
@@ -1,19 +1,42 @@
+import {Button,Card} from 'react-bootstrap';
 import DataFoods from "./Data/DataFoods";
+
 const NsCustomer =()=>{
     return(
         <>
         <div>Customer</div>
-        <p>{DataFoods.map((foods,id)=>{
-            return(
-                <div key={id}> 
-                    <img src={foods.image}/>
-                    <p>{foods.title}</p>
-                    <p>{foods.type}</p>
-                    <p>{foods.show}</p>
-                    <p>{foods.total}</p>
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-10'>
+                        <div className='container-fluid'>
+                            <div className='row'>
+                                {DataFoods.map((foods,id)=>{
+                                    return(
+                                            <div key={id} className='col col-xl-3 col-xxl-2'>
+                                                <Card style={{ width: '15.6rem' }} className='m-1'>
+                                                    <Card.Img variant="top" style={{ width: '12.5rem' }} className='mx-auto' src={foods.image} />
+                                                    <Card.Body>
+                                                        <Card.Title style={{ fontSize: '1.5rem' }} className='text-center'>
+                                                            {foods.title}
+                                                        </Card.Title>
+                                                        <Button variant="primary" style={{ width: '12.5rem' }}>เพิ่ม</Button>
+                                                    </Card.Body>
+                                                </Card>
+                                            </div>
+                                        
+                                        )
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                <div className='col-2'>
+                                <h1>Your Order</h1>
                 </div>
-            )
-        })}</p>
+            </div>
+        </div>
+            
+        
+
         </>
     )
 }
